3f8f7f69484b3724d74846d410ffafa8774c1341
Kenny Chirombo <chirombokenny@gmail.com>
2024-07-12 12:55:54 +0200

    add auth controller and routes

---
+++ backend/src/controllers/authController.ts
@@ -0,0 +1,14 @@
+import {Request, Response} from "express";
+import {asyncHandler} from "../utils/asyncHandler";
+import { authService } from "@services/AuthService";
+
+export const signup = asyncHandler(async (req: Request, res: Response) => {
+  const {user, token} = await authService.signup(req.body);
+  res.status(201).json({user, token});
+});
+
+export const signin = asyncHandler(async (req: Request, res: Response) => {
+  const {email, password} = req.body;
+  const {user, token} = await authService.signin(email, password);
+  res.json({user, token});
+});
--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,8 +1,11 @@
+import { signin, signup } from "@controllers/authController";
+import { requestValidator } from "@middlewares/requestValidator";
+import { signinSchema, signupSchema } from "@utils/validation";
 import express from "express";
 
 const router = express.Router();
 
-router.post("/signup");
-
+router.post("/signup", requestValidator(signupSchema), signup);
+router.post("/signin", requestValidator(signinSchema), signin);
 
 export default router;
