1a57815c40024b54431a2786cdb3373fa71a6f93
Kenny Chirombo <chirombokenny@gmail.com>
2024-07-12 12:59:17 +0200

    updated main app.ts

--- backend/app.ts
+++ backend/app.ts
@@ -1,13 +1,20 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import {config} from "@config/config";
+import authRoutes from "@routes/authRoutes";
+import { errorHandler } from "@middlewares/errorHandler";
+import { config } from "@config/config";
+
 
 const app = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.use(errorHandler);
 
 const PORT = config.PORT;
 
